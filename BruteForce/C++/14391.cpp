70fdf2dd4da0ba893ea4e74d16ad33190f4e2f81
acver <cmak139@gmail.com>
2021-08-26 17:22:19 +0900

    2021.08.26

--- BruteForce/C++/14391.cpp
+++ BruteForce/C++/14391.cpp
@@ -1,27 +1,49 @@
 //2021.08.25
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main(){
     int n, m;
-    char a[4][4];
-    char v[16];
-    char z = '0';
+    int a[4][4];
+    int ans = 0;
     cin >> n >> m;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-            v[i*m+j] = a[i][j];
+            char c;
+            cin >> c;
+            a[i][j] = c - '0';
         }
     }
-
-    for(int i = 0; i < n*m; i++){
-        cout << v[i] << ' ';
-    }
-
-    for(int i = 0; i < n; i++){
+    for(int s = 0; s < (1<<(n*m)); s++){
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            int cur = 0;
+            for(int j = 0; j < m; j++){
+                int k = i*m+j;
+                if((s&(1<<k)) == 0){
+                    cur = cur * 10 + a[i][j];
+                }else{
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+        }
         for(int j = 0; j < m; j++){
-
+            int cur = 0;
+            for(int i = 0; i < n; i++){
+                int k = i*m+j;
+                if((s&(1<<k)) != 0){
+                    cur = cur * 10 + a[i][j];
+                }else{
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
         }
+        ans = max(ans, sum);
     }
+    cout << ans;
 }
