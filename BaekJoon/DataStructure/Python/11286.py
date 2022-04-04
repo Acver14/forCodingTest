4c87b8d7ce6fbc8b030a624e07ae02994f34b208
Acver <devacver@gmail.com>
2022-04-04 17:01:47 +0900

    [add] 2022.04.04

---
+++ BaekJoon/DataStructure/Python/11286.py
@@ -0,0 +1,15 @@
+import heapq
+from sys import stdin
+input = stdin.readline
+
+heap = []
+n = int(input().rstrip())
+for _ in range(n):
+    x = int(input().rstrip())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (abs(x), x))
--- BaekJoon/DataStructure/Python/14425.py
+++ BaekJoon/DataStructure/Python/14425.py
@@ -2,7 +2,9 @@ from sys import stdin
 input = stdin.readline
 
 n, m = map(int, input().strip().split())
-string = input().strip()
+string = []
+for i in range(n):
+    string.append(input().strip())
 
 ans = 0
 for i in range(m):
@@ -11,3 +13,9 @@ for i in range(m):
         ans += 1
     
 print(ans)
+
+# import sys
+# I=sys.stdin.readline
+# N,M=map(int,I().split())
+# A={I().rstrip() for _ in [0]*N}
+# print(sum([1*(I().rstrip() in A) for _ in [0]*M]))
