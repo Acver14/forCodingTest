7faa6c5953cbbd5836a4c3f1ead524b98f95996c
Acver <devacver@gmail.com>
2022-09-20 20:43:43 +0900

    2022.09.20

--- 2022/09/21_week/boj/20159.py
+++ 2022/09/21_week/boj/20159.py
@@ -1,34 +1,28 @@
 from collections import deque
-
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
 N = int(input())
 card = list(map(int, input().split()))
-card = deque(card)
 ans = 0
 
-def back(flag, sum1, sum2, card_list):
+def func(flag, sum1, sum2, card_list, front, back):
     global ans
-    if card_list:
+    if front < back:
         if flag:
-            num1, num2 = card_list.pop(), card_list.pop()
-            back(flag, sum1+num1, sum2+num2, card_list)
-            card_list.append(num1)
-            card_list.append(num2)
+            num1, num2 = card_list[back], card_list[back-1]
+            func(flag, sum1+num1, sum2+num2, card_list, front, back-2)
         else:
-            num1, num2 = card_list.pop(), card_list.pop()
-            back(flag, sum1+num1, sum2+num2, card_list)
-            card_list.append(num1)
-            card_list.append(num2)
+            num1, num2 = card_list[back], card_list[back-1]
+            func(flag, sum1+num1, sum2+num2, card_list, front, back-2)
             
-            num1, num2 = card_list.popleft(), card_list.pop()
-            back(True, sum1+num1, sum2+num2, card_list)
-            card_list.appendleft(num1)
-            card_list.append(num2)
+            num1, num2 = card_list[front], card_list[back]
+            func(True, sum1+num1, sum2+num2, card_list, front+1, back-1)
             
-            num1, num2 = card_list.pop(), card_list.popleft()
-            back(True, sum1+num1, sum2+num2, card_list)
-            card_list.append(num1)
-            card_list.appendleft(num2)
+            num1, num2 = card_list[back], card_list[front]
+            func(True, sum1+num1, sum2+num2, card_list, front+1, back-1)
     else:
         ans = max(ans, sum1)
-back(False, 0, 0, card)
+        
+func(False, 0, 0, card, 0, N-1)
 print(ans)
