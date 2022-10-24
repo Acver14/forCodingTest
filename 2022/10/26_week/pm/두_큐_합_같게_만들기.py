8a25eebc54fe987fcf8ee2fdd0d00150c58295e6
Acver <devacver@gmail.com>
2022-10-24 16:20:45 +0900

    2022.10.24

--- 2022/10/26_week/pm/두_큐_합_같게_만들기.py
+++ 2022/10/26_week/pm/두_큐_합_같게_만들기.py
@@ -1,24 +1,18 @@
-from collections import deque
 def solution(queue1, queue2):
-    global answer
     answer = 0
-    ans1 = func(deque(queue1.copy()), deque(queue2.copy()))
-    ans2 = func(deque(queue2.copy()), deque(queue1.copy()))
-    print(ans1, ans2)
-    if ans1 == -1:
-        answer = ans2
-    elif ans2 == -1:
-        answer = ans1
-    else:
-        answer = min(ans1, ans2)
-    return answer
-
-def func(queue1, queue2):
-    ret = 0
-    while queue2:
-        print(queue1, queue2, sum(queue1), sum(queue2))
-        if sum(queue1) == sum(queue2):
-            return ret
-        queue1.append(queue2.popleft())
-        ret += 1
+    queue = queue1 + queue2
+    _sum = sum(queue1)
+    target = sum(queue) // 2
+    i, j = 0, len(queue1) - 1
+    
+    while i < len(queue) and j < len(queue):
+        if _sum == target:
+            return answer
+        elif _sum < target and j < len(queue) - 1:
+            j += 1
+            _sum += queue[j]
+        else:
+            _sum -= queue[i]
+            i += 1
+        answer += 1
     return -1
